41ffd26640cb3aa25559e39e999f922360fe722d
Migamix <migamix@hotmail.com>
2017-02-15 21:36:32 -0600

    move around some programming tools order
    attempt to remove issue of browser resource cant open 2 pages

--- linux_newapps.sh
+++ linux_newapps.sh
@@ -18,18 +18,18 @@ sudo apt-get install gimp -y
 #sudo apt-get install mixxx -y
 
 #programming tools #old version on RPi
-sudo apt-get install arduino arduino-core -y
-sudo apt-get install codeblocks -y
 echo "you can download some updated files while you wait"
 echo " and install them on top of the auto downloaded files"
-xdg-open https://www.arduino.cc/en/Main/Software &
-xdg-open http://www.codeblocks.org/ &
+xdg-open https://www.arduino.cc/en/Main/Software & http://www.codeblocks.org/ &
+sudo apt-get install arduino arduino-core -y
+sudo apt-get install codeblocks -y
+sudo apt-get install gedit -y
+sudo apt-get install geany -y
+
 
 #system tools
 sudo apt-get install gnome-schedule -y
 sudo apt-get install gparted -y
-sudo apt-get install gedit -y
-sudo apt-get install geany -y
 sudo apt-get install gdebi -y
 sudo apt-get install cifs-utils -y
 sudo apt-get install ntp -y
@@ -53,7 +53,7 @@ sudo apt-get install pulseaudio-module-bluetooth -y
 # sudo apt-get install at-spi2-core -y
 
 sudo apt-get dist-upgrade
-# below for raspbery pi only --------------------------------------------
+# below for raspbery pi only (will fail if not on RPI)
 sudo apt-get install rpi-update -y
 sudo rpi-update
 sudo apt-get install pi-bluetooth -y

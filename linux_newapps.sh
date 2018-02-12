54937625607c7a8ba074a2667e53f268a7a8fadb
Migamix <migamix@hotmail.com>
2018-02-12 16:18:01 -0600

    Update linux_newapps.sh
    cleaned up for individual package base

--- linux_newapps.sh
+++ linux_newapps.sh
@@ -2,11 +2,11 @@
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
-#sudo apt-get autoremove -y
+sudo apt-get autoremove -y
 
 #network tools
-#sudo apt-get install firefox-esr -y
-#sudo apt-get install icedove -y
+sudo apt-get install firefox-esr -y
+sudo apt-get install icedove -y
 sudo apt-get install filezilla -y
 
 #media tools
@@ -18,7 +18,7 @@ sudo apt-get install jackd qjackctl -y
 sudo apt-get install mixxx -y
 
 #programming tools #old version on RPi
-sudo apt-get install arduino arduino-core -y
+sudo apt-get install arduino arduino-core -y # verify this installs the latest version
 sudo apt-get install codeblocks -y
 sudo apt-get install gedit -y
 sudo apt-get install geany -y
@@ -35,10 +35,12 @@ sudo apt-get install system-config-samba -y
 
 #directory tools
 sudo apt-get install caja -y
-# sudo apt-get install xfe -y
-#sudo apt-get install dolphin -y
+# other directory options below
+sudo apt-get install xfe -y
+sudo apt-get install dolphin -y
 
 #bluetooth audio tools
+sudo apt-get install pi-bluetooth
 sudo apt-get install bluez -y
 sudo apt-get install bluez-firmware -y
 sudo apt-get install blueman -y
@@ -47,14 +49,13 @@ sudo apt-get install pavucontrol -y
 sudo apt-get install pulseaudio-module-bluetooth -y
 
 #virtual keyboard
-# sudo apt-get install florence -y
-# sudo apt-get install at-spi2-core -y
+sudo apt-get install florence -y
+sudo apt-get install at-spi2-core -y
 
 sudo apt-get dist-upgrade
-# below for raspbery pi only (will fail if not on RPI)
+# below for raspbery pi only (will fail to do anything if not on RPI)
 sudo apt-get install rpi-update -y
-sudo rpi-update
-sudo apt-get install pi-bluetooth -y
+sudo rpi-update -y
 
 read -p "press a button to reboot, or shut down this script" 
-sudo reboot
+sudo shutdown -h

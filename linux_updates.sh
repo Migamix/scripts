4b9a77f1dc17e1b1eaebd97ed8a1bcfa7ccb80d5
Migamix <migamix@hotmail.com>
2019-02-04 13:46:49 -0600

    apt-get to apt

--- linux_updates.sh
+++ linux_updates.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #sudo apt-get autoremove
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade
-sudo apt-get install rpi-update -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt dist-upgrade
+sudo apt install rpi-update -y
 sudo rpi-update
 read -p "press a button to reboot, or shut down this script" 
 sudo reboot

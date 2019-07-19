19b6e2ffbbf6fde8e1cf6757cfba8dd7b54410c7
Migamix <migamix@hotmail.com>
2019-07-18 21:35:55 -0500

    add matchboox, remove "-get"

--- linux_newapps.sh
+++ linux_newapps.sh
@@ -1,60 +1,61 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get autoremove -y
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt autoremove -y
 
 #network tools
-sudo apt-get install firefox-esr -y
-sudo apt-get install icedove -y
-sudo apt-get install filezilla -y
+sudo apt install firefox-esr -y
+sudo apt install icedove -y
+sudo apt install filezilla -y
 
 #media tools
-sudo apt-get install mplayer2 -y
-sudo apt-get install gimp -y
-sudo apt-get install lmms -y
-sudo apt-get install audacity -y
-sudo apt-get install jackd qjackctl -y
-sudo apt-get install mixxx -y
+sudo apt install mplayer2 -y
+sudo apt install gimp -y
+sudo apt install lmms -y
+sudo apt install audacity -y
+sudo apt install jackd qjackctl -y
+sudo apt install mixxx -y
 
 #programming tools #old version on RPi
-sudo apt-get install arduino arduino-core -y # verify this installs the latest version
-sudo apt-get install codeblocks -y
-sudo apt-get install gedit -y
-sudo apt-get install geany -y
+sudo apt install arduino arduino-core -y # verify this installs the latest version
+sudo apt install codeblocks -y
+sudo apt install gedit -y
+sudo apt install geany -y
 
 
 #system tools
-sudo apt-get install gnome-schedule -y
-sudo apt-get install gparted -y
-sudo apt-get install gdebi -y
-sudo apt-get install cifs-utils -y
-sudo apt-get install ntp -y
-sudo apt-get install htop -y
-sudo apt-get install system-config-samba -y
+sudo apt install gnome-schedule -y
+sudo apt install gparted -y
+sudo apt install gdebi -y
+sudo apt install cifs-utils -y
+sudo apt install ntp -y
+sudo apt install htop -y
+sudo apt install system-config-samba -y
 
 #directory tools
-sudo apt-get install caja -y
+sudo apt install caja -y
 # other directory options below
-sudo apt-get install xfe -y
-sudo apt-get install dolphin -y
+#sudo apt-get install xfe -y
+#sudo apt-get install dolphin -y
 
 #bluetooth audio tools
-sudo apt-get install pi-bluetooth
-sudo apt-get install bluez -y
-sudo apt-get install bluez-firmware -y
-sudo apt-get install blueman -y
-sudo apt-get install pulseaudio -y
-sudo apt-get install pavucontrol -y
-sudo apt-get install pulseaudio-module-bluetooth -y
+sudo apt install pi-bluetooth
+sudo apt install bluez -y
+sudo apt install bluez-firmware -y
+sudo apt install blueman -y
+sudo apt install pulseaudio -y
+sudo apt install pavucontrol -y
+sudo apt install pulseaudio-module-bluetooth -y
 
-#virtual keyboard
-sudo apt-get install florence -y
-sudo apt-get install at-spi2-core -y
+#virtual sudo apt-get install matchbox-keyboardkeyboard
+sudo apt install matchbox-keyboard
+sudo apt install florence -y
+sudo apt install at-spi2-core -y
 
-sudo apt-get dist-upgrade
+sudo apt dist-upgrade
 # below for raspbery pi only (will fail to do anything if not on RPI)
-sudo apt-get install rpi-update -y
+sudo apt install rpi-update -y
 sudo rpi-update -y
 
 read -p "press a button to reboot, or shut down this script" 

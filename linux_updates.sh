b9feab6ef62cae2c5d2493c07d261ee707c8e184
Migamix <migamix@hotmail.com>
2019-07-19 14:31:29 -0500

    removed rpi-update from ex
    WARNING: 'rpi-update' updates to pre-releases of the linux 
    kernel tree and Videocore firmware.
    
    'rpi-update' should only be used if there is a specific 
    reason to do so - for example, a request by a Raspberry Pi 
    engineer.
    
    DO NOT use 'rpi-update' as part of a regular update process.

--- linux_updates.sh
+++ linux_updates.sh
@@ -5,6 +5,6 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt dist-upgrade
 sudo apt install rpi-update -y
-sudo rpi-update
+#sudo rpi-update #removed from reg updates
 read -p "press a button to reboot, or shut down this script" 
 sudo shutdown -r +1

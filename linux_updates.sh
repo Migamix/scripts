8c2ae6a3ec90c673f06fcf874e2f64957a332ec8
Migamix <migamix@hotmail.com>
2019-07-19 21:35:22 -0500

    Update linux_updates.sh

--- linux_updates.sh
+++ linux_updates.sh
@@ -6,5 +6,5 @@ sudo apt upgrade -y
 sudo apt dist-upgrade
 sudo apt install rpi-update -y
 #sudo rpi-update #removed from reg updates
-read -p "press a button to reboot, or shut down this script" 
+read -p "press a button to reboot, or shut down this script (ctrl+c)" 
 sudo shutdown -r +1

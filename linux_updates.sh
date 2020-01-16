9507b467f518a13ba5f646a2633f796edde0be8d
Migamix <migamix@hotmail.com>
2020-01-16 17:27:35 -0600

    Update linux_updates.sh
    recon

--- linux_updates.sh
+++ linux_updates.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#sudo apt-get autoremove
+sudo apt-get autoremove
 sudo apt update
 sudo apt upgrade -y
 sudo apt dist-upgrade
 sudo apt install rpi-update -y
-#sudo rpi-update #removed from reg updates
-read -p "press a button to reboot, or shut down this script (ctrl+c)" 
+sudo rpi-update 
+read -p "press a button to reboot, or shut down this script (ctrl+c)" #may be skipped during cron job
 sudo shutdown -r +1
